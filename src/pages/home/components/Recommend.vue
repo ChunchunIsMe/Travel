d76aba6bef96af2d51f561bf359ce013ecd036d6
ChunchunIsMe <843799751@qq.com>
2018-08-11 20:46:54 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <img class="item-img" :src="item.imgUrl" alt="">
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeRecommend',
+    data () {
+        return {
+            recommendList: [{
+                id: '0001',
+                imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg',
+                title: '大连圣亚海洋世界',
+                desc: '浪漫大连首站，浪漫的海洋主题乐园'
+            },{
+                id: '0002',
+                imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg',
+                title: '大连圣亚海洋世界',
+                desc: '浪漫大连首站，浪漫的海洋主题乐园'
+            },{
+                id: '0003',
+                imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg',
+                title: '大连圣亚海洋世界',
+                desc: '浪漫大连首站，浪漫的海洋主题乐园'
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/mixins.styl'
+    .title
+        margin-top .2rem
+        line-height .8rem
+        background #eee
+        text-indent .2rem
+    .item
+        display flex
+        overflow hidden
+        height 1.9rem
+        .item-img
+            width 1.7rem
+            height 1.7rem
+            padding .1rem
+        .item-info
+            flex 1
+            padding .1rem
+            min-width 0
+            .item-title
+                line-height .54rem
+                font-size .32rem
+                ellipsis()
+            .item-desc
+                line-height .4rem
+                color #ccc
+                ellipsis()
+            .item-button 
+                line-height .44rem
+                margin-top .16rem
+                background #ff9300
+                padding 0 .2rem
+                border-radius .06rem
+                color #fff
+</style>
+
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.imgUrl" alt="">
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeWeekend',
+    data () {
+        return {
+            recommendList: [{
+                id: '0001',
+                imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+                title: '大连圣亚海洋世界',
+                desc: '浪漫大连首站，浪漫的海洋主题乐园'
+            },{
+                id: '0002',
+                imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+                title: '大连圣亚海洋世界',
+                desc: '浪漫大连首站，浪漫的海洋主题乐园'
+            },{
+                id: '0003',
+                imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+                title: '大连圣亚海洋世界',
+                desc: '浪漫大连首站，浪漫的海洋主题乐园'
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/mixins.styl'
+    .title
+        margin-top .2rem
+        line-height .8rem
+        background #eee
+        text-indent .2rem
+    .item-img-wrapper
+        overflow hidden
+        height 0
+        padding-bottom 33.9%
+        .item-img
+            width 100%
+    .item-info
+        padding .1rem
+        min-width 0
+        .item-title
+            line-height .54rem
+            font-size .32rem
+            ellipsis()
+        .item-desc
+            line-height .4rem
+            color #ccc
+            ellipsis()
+</style>
+
