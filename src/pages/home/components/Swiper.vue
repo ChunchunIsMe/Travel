74971677a755a3f190e53701bbfd2c4558cf41e5
ChunchunIsMe <843799751@qq.com>
2018-08-11 11:27:57 +0800

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" alt="">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+    
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true
+      },
+      swiperList: [{
+          id: '0001',
+          imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+      },{
+          id: '0002',
+          imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png'
+      }]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+    // 
+    .wrapper >>> .swiper-pagination-bullet-active
+        background-color #fff !important
+    .wrapper
+        overflow hidden
+        width 100%
+        height 0
+        padding-bottom 24.53%
+        background-color #ccc
+        .swiper-img
+            width 100%
+</style>
