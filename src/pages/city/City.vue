bfeba9886bae649b099bf3a897ebbf50ba035272
ChunchunIsMe <843799751@qq.com>
2018-08-13 15:47:28 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hot="hotCities"></city-list>
+        <city-alphabet  :cities="cities"></city-alphabet>
     </div>
 </template>
 
@@ -12,6 +12,7 @@ import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 export default {
     name: 'City',
     components: {
@@ -19,6 +20,30 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    data () {
+        return {
+            cities: {},
+            hotCities: []
+        }
+    },
+    methods: {
+        getCityInfo () {
+            axios.get('/api/city.json')
+                .then(this.handleGetCItyInfoSucc)
+        },
+        handleGetCItyInfoSucc (res) {
+            res = res.data
+            if (res.ret && res.data) {
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+            console.log(res);
+        }
+    },
+    mounted () {
+        this.getCityInfo()
     }
 }
 </script>
