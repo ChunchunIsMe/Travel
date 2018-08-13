032274fbc2abb682b81446631f5aaecc64a7b170
ChunchunIsMe <843799751@qq.com>
2018-08-13 15:19:49 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl';
+    .list
+        display flex
+        flex-direction column
+        justify-content center
+        position absolute
+        right 0
+        top 1.58rem
+        bottom 0
+        width .4rem
+        .item
+            line-height .44rem
+            color $bgColor
+            text-align center
+</style>
