a9b0a7f1febf840fb35706257d493ffaa20cc515
ChunchunIsMe <843799751@qq.com>
2018-08-14 21:16:44 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" src="http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg" alt="">
+            <div class="banner-info">
+                <div class="banner-title">
+                    大连圣亚海洋世界(AAAA景区)
+                </div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe616;</span>
+                    39
+                </div>
+            </div>
+        </div>
+        <common-gallary :imgs='imgs' v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </div>
+    
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+    name: 'DetailBanner',
+    data () {
+        return {
+            showGallary: false,
+            imgs:['http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg','http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg']
+        }
+    },
+    methods: {
+        handleBannerClick () {
+            this.showGallary = true
+        },
+        handleGallaryClose () {
+            this.showGallary = false
+        }
+    },
+    components: {
+        CommonGallary
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .banner
+        position relative
+        overflow hidden
+        height 0
+        padding-bottom 55%
+        background-color red   
+        .banner-img
+            width 100%
+        .banner-info
+            display flex
+            position absolute
+            left 0
+            right 0
+            bottom 0
+            line-height .6rem
+            color #fff
+            background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+            .banner-title
+                font-size .32rem
+                padding 0 .2rem
+                flex 1
+            .banner-number
+                margin-top .14rem
+                padding  0 .4rem
+                height .32rem
+                line-height .32rem
+                font-size .24rem
+                border-radius .2rem
+                background rgba(0, 0, 0, .8)
+                .banner-icon
+                    font-size .24rem
+</style>
