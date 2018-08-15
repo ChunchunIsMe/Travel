42a37687fdfd50b59c9985f400b5163cb09f9e1b
ChunchunIsMe <843799751@qq.com>
2018-08-15 16:08:39 +0800

    breake

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -9,11 +9,30 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import axios from 'axios'
 export default {
     name: 'Detail',
     components: {
         DetailBanner,
         DetailHeader
+    },
+    methods: {
+        getDetailInfo () {
+            axios.get('/api/detail.json',{
+                params: {
+                    id: this.$route.params.id
+                }
+            }).then(this.handleGetDataSucc)
+        },
+        handleGetDataSucc (res) {
+            res = res.data
+            if (res.ret && res.data) {
+                const data = res.data
+            }
+        }
+    },
+    mounted () {
+        this.getDetailInfo()
     }
 }
 </script>
